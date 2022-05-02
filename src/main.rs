6d2f72f172ec1ed8995ba2a7565508eeca42d5bb
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-05-02 16:34:39 +0200

    Simplify listener and add transaction content extraction
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,47 @@
-use std::{env, future, process};
+use log::{error, warn};
+use log::info;
+
+use std::{env, process};
 use std::env::VarError;
-use web3::futures::StreamExt;
+
+use web3::futures::{TryStreamExt};
 use web3::transports::{WebSocket};
+use web3::types::{TransactionId};
 
 fn get_node_endpoint() -> Result<String, VarError> {
-    env::var("NODE_ENDPOINT")
+        env::var("WSS_NODE_ENDPOINT")
 }
 
 #[tokio::main]
 async fn main() -> web3::Result {
-    let node_endpoint = match get_node_endpoint() {
+    env_logger::init();
+
+    let wss_node_endpoint = match get_node_endpoint() {
         Ok(r) => r,
         Err(e) => {
-            println!("Failed to start listener: {}", e);
+            error!("{:?}", e);
             process::exit(1)
         },
     };
+    let sub_transport = WebSocket::new(wss_node_endpoint.as_str()).await?;
+    let web3 = web3::Web3::new(sub_transport);
 
-    let sub_transport = WebSocket::new(node_endpoint.as_str()).await?;
-    let subscriber = web3::Web3::new(sub_transport);
+    let mut pending_transactions = web3.eth_subscribe().subscribe_new_pending_transactions().await?;
 
-    /*
-    let clt_transport = Http::new(node_endpoint.as_str())?;
-    let clt = web3::Web3::new(clt_transport);
-     */
+    while let Some(pending_transaction_hash) = pending_transactions.try_next().await? {
+        let pth = TransactionId::from(pending_transaction_hash);
 
-    let pending_transactions = subscriber.eth_subscribe().subscribe_new_pending_transactions().await?;
-
-    pending_transactions.for_each(|pending_txn| {
-        println!("{}", pending_txn.unwrap());
-        future::ready(())
-    }).await;
+        let res = web3.eth().transaction(pth).await;
+        match res {
+            Ok(opt_txn) => {
+                match opt_txn {
+                    None => { warn!("could not find transaction for now") },
+                    Some(txn) => info!("{:?}", txn)
+                }
+            }
+            Err(e) => error!("{:?}", e)
+        }
+    }
 
     Ok(())
 }
